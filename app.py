46743ee1a113e8504836773add29ad3a80669133
heyanil24 <158294036+heyanil24@users.noreply.github.com>
2024-02-02 23:06:16 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,9 @@
+import json
+
+def lambda_handler(event, context):
+    site_health_score = 90   (  logic )
+    return  {
+        "statusCode": 200,
+       'body': json.dumps({'site_health_score': site_health_score_value})
+    }
+    return response
